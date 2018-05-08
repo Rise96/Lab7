e115ad086291999d8d6f54f6743ca4b765e81387
Rise <ritm-temp@hotmail.com>
2018-05-08 10:52:17 +0600

    Lab7 v1.0

---
+++ Lab7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new List<List<int>>();
+            var row = new List<int>(); 
+            var rand = new Random();
+            int n = rand.Next(3,6);
+
+            for (int i = 0; i < n; i++)
+            {
+                row = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    row.Add(rand.Next(-30,30));
+                }
+
+                array.Add(row);                               
+            }
+            
+            for (int i = 0; i < n; i++)                    
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i][j].ToString() + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Rearrangement rearrangement = new Rearrangement();
+            List<List<int>> array2 = rearrangement.Rearrange(array);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array2[i][j].ToString() + " ");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab7/Rearrangement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Lab7
+{
+    class Rearrangement
+    {
+        public List<List<int>> Rearrange(List<List<int>> array)
+        {
+            int indexOf1stColumn = FindColumnWithSecondEven(array);
+            int indexOf2ndColumn = FindColumnWirhNegative(array);
+            for(int i = 0; i < array.Count; i++)
+            {
+                int temporary = array[i][indexOf1stColumn];
+                array[i].RemoveAt(indexOf1stColumn);
+                array[i].Insert(indexOf1stColumn, array[i][indexOf2ndColumn-1]);
+                
+                array[i].RemoveAt(indexOf2ndColumn);
+                array[i].Insert(indexOf2ndColumn, temporary);
+
+            }
+            return array;
+        }
+
+        public int FindColumnWithSecondEven(List<List<int>> array)
+        {
+            int index = 0;
+            for(int j = 0;j< array[1].Count ; j++)
+            {
+                if (array[1][j] % 2 == 0)
+                {
+                    index = j;
+                    break;
+                }
+            }
+            return index;
+        }
+
+        public int FindColumnWirhNegative(List<List<int>> array)
+        {
+            int index = 0;
+            for (int j = array[0].Count-1; j >0; j--)
+            {
+                if (array[0][j]<0)
+                {
+                    index = j;
+                    break;
+                }
+                else
+                {
+                    index = 0;
+                }
+            }
+            return index;
+        }
+    }
+}
